ecb7dd7c9c03e571c4958709dcd0c8cb1c84040a
David Gonzalez <62410785+DavidGlezQ@users.noreply.github.com>
2023-03-13 12:45:31 -0600

    Seccion8, check timezone

--- ForecastWeather/app/src/test/java/com/cursosandroidant/forecastweather/common/dataAccess/ResponseServerTest.kt
+++ ForecastWeather/app/src/test/java/com/cursosandroidant/forecastweather/common/dataAccess/ResponseServerTest.kt
@@ -1,5 +1,6 @@
 package com.cursosandroidant.forecastweather.common.dataAccess
 
+import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
 import org.hamcrest.CoreMatchers.*
 import org.hamcrest.MatcherAssert.assertThat
@@ -8,6 +9,7 @@ import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.junit.MockitoJUnitRunner
+import java.net.HttpURLConnection
 
 /**
  * Created by davidgonzalez on 13/03/23
@@ -32,5 +34,15 @@ class ResponseServerTest {
         val reader = JSONFileLoader().loadJSONString("weather_forecast_response_success.json")
         assertThat(reader, `is`(notNullValue()))
         assertThat(reader, containsString("America/Mexico_City"))
+    }
+
+    @Test
+    fun getWeatherForecastTimezoneExist() {
+        val response = MockResponse()
+            .setResponseCode(HttpURLConnection.HTTP_OK)
+            .setBody(JSONFileLoader().loadJSONString("weather_forecast_response_success.json") ?: "{errorCode:34}")
+        mockWebServer.enqueue(response)
+
+        assertThat(response.getBody()?.readUtf8(), containsString("\"timezone\""))
     }
 }
